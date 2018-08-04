6e32a0fe3badb02405ece52d8992cdba0c048751
binarydylan <wishnerdylan@gmail.com>
2018-08-04 13:33:14 -0700

    Initial commit

---
+++ src/main/java/binarydylan/custompcb/CustomPCB.java
@@ -0,0 +1,8 @@
+package binarydylan.custompcb;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class CustomPCB {
+
+}
---
+++ src/main/java/binarydylan/custompcb/Reference.java
@@ -0,0 +1,8 @@
+package binarydylan.custompcb;
+
+public class Reference {
+    // Feel free to change these if you can come up with a better name
+    public static final String MODID = "custompcb";
+    public static final String NAME = "CustomPCB";
+    public static final String VERSION = "super-duper alpha 0.00001";
+}
