de6b93be268910ac2e8e7c5ed4b40cf9f753ef2f
Master Maniac <barrettseamonkey@gmail.com>
2018-08-08 12:33:55 -0500

    More setup stuff, and added an item
    Mostly just getting organized. Totally didn't have to go back to some modding tutorials to remember what I was supposed to do, I promise.

--- src/main/java/binarydylan/custompcb/Items/ModItems.java
+++ src/main/java/binarydylan/custompcb/Items/ModItems.java
@@ -1,5 +1,16 @@
 package binarydylan.custompcb.Items;
 
+import binarydylan.custompcb.Items.ItemBase;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.item.Item;
+
 public class ModItems {
 		//class for creating items in general
+	
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	
+	public static final Item SINISTER_ALLOY = new ItemBase("sinister_alloy");
+	
 }
---
+++ src/main/java/binarydylan/custompcb/util/IHasModel.java
@@ -0,0 +1,5 @@
+package binarydylan.custompcb.util;
+
+public interface IHasModel {
+	public void registerModels();
+}
